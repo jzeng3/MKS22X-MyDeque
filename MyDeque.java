a773333e3cf671296481351df0dcd995b6c8e4ea
Jamie Zeng <jzeng3@stuy.edu>
2019-03-21 09:53:17 -0400

    started code for addFirst

--- MyDeque.java
+++ MyDeque.java
@@ -10,6 +10,7 @@ public class MyDeque<E>{
       start = 0;
       end = 0;
      }
+
   public MyDeque(int initialCapacity){
     @SuppressWarnings("unchecked")
       E[] d = (E[])new Object[initialCapacity];
@@ -18,9 +19,11 @@ public class MyDeque<E>{
       start = 0;
       end = 0;
     }
+
   public int size(){
    return size;
  }
+
   public String toString(){
     String dataStr = "";
     dataStr += "[";
@@ -48,10 +51,26 @@ public class MyDeque<E>{
     dataStr += "]";
     return dataStr;
   }
-  public void addFirst(E element){ }
+
+  public void addFirst(E element){
+    if (start == 0 && size < data.length){
+      start = data.length - 1;
+    }
+    else if (start > 0 && size < data.length){
+      start--;
+    }
+    else if (size == data.length){
+
+    }
+    data[start] = element;
+    size++;
+  }
   public void addLast(E element){ }
-  public E removeFirst(E element){ return element;}
-  public E removeLast(E element){return element; }
-  public E getFirst(E element){ return element;}
-  public E getLast(E element){return element; }
+  public E removeFirst(){ return element;}
+  public E removeLast(){return element; }
+  public E getFirst(){ return element;}
+  public E getLast(){return element; }
+  private void resize(E[] array){
+    
+  }
 }
