3517ab0e25abbca5344e4d8ef3d34a1c5fe5012a
Jamie Zeng <jzeng3@stuy.edu>
2019-03-21 09:41:57 -0400

    coded toString code

--- MyDeque.java
+++ MyDeque.java
@@ -7,21 +7,51 @@ public class MyDeque<E>{
       E[] d = (E[])new Object[10];
       data = d;
       size = 0;
+      start = 0;
+      end = 0;
      }
   public MyDeque(int initialCapacity){
     @SuppressWarnings("unchecked")
       E[] d = (E[])new Object[initialCapacity];
       data = d;
       size = 0;
+      start = 0;
+      end = 0;
     }
   public int size(){
    return size;
  }
-  public String toString(){ }
+  public String toString(){
+    String dataStr = "";
+    dataStr += "[";
+    if (size > 0){
+      if (start <= end){
+      for (int i = start; i <= end; i++){
+        dataStr += data[i];
+        if (i != end){
+          dataStr += ", ";
+        }
+      }
+    }
+      else{
+        for (int i = start; i < data.length; i++){
+          dataStr += data[i] + ", ";
+        }
+        for (int i = 0; i <= end; i++){
+          dataStr += data[i];
+          if (i != end){
+            dataStr += ", ";
+          }
+        }
+      }
+    }
+    dataStr += "]";
+    return dataStr;
+  }
   public void addFirst(E element){ }
   public void addLast(E element){ }
-  public E removeFirst(E element){ }
-  public E removeLast(E element){ }
-  public E getFirst(E element){ }
-  public E getLast(E element){ }
+  public E removeFirst(E element){ return element;}
+  public E removeLast(E element){return element; }
+  public E getFirst(E element){ return element;}
+  public E getLast(E element){return element; }
 }
