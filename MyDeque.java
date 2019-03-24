91856950708ba14946f8da6bd5b791c8869f83f8
Jamie Zeng <jzeng3@stuy.edu>
2019-03-23 22:38:14 -0400

    tested removeLast, added comments

--- MyDeque.java
+++ MyDeque.java
@@ -7,26 +7,26 @@ public class MyDeque<E>{
   // initiate deque with an empty array of 10 elements, set size, start, end to 0 by default
   public MyDeque(){
     @SuppressWarnings("unchecked")
-      E[] d = (E[])new Object[10];
-      data = d;
-      size = 0;
-      start = 0;
-      end = 0;
-     }
-// initiate deque with an empty array of specified capacity, set size, start, end to 0 by default
+    E[] d = (E[])new Object[10];
+    data = d;
+    size = 0;
+    start = 0;
+    end = 0;
+  }
+  // initiate deque with an empty array of specified capacity, set size, start, end to 0 by default
   public MyDeque(int initialCapacity){
     @SuppressWarnings("unchecked")
-      E[] d = (E[])new Object[initialCapacity];
-      data = d;
-      size = 0;
-      start = 0;
-      end = 0;
-    }
+    E[] d = (E[])new Object[initialCapacity];
+    data = d;
+    size = 0;
+    start = 0;
+    end = 0;
+  }
 
   // return number of elements in deque
   public int size(){
-   return size;
- }
+    return size;
+  }
 
   // for printing out deque
   public String toString(){
@@ -35,15 +35,15 @@ public class MyDeque<E>{
     if (size > 0){
       // if start is less than end, just add every element from start to end
       if (start <= end){
-      for (int i = start; i <= end; i++){
-        dataStr += data[i];
-        if (i != end){
-          dataStr += ", ";
+        for (int i = start; i <= end; i++){
+          dataStr += data[i];
+          if (i != end){
+            dataStr += ", ";
+          }
         }
       }
-    }
-     /* if array "loops around," add elements from START of deque to end of the array,
-        then from beginning to END of deque */
+      /* if array "loops around," add elements from START of deque to end of the array,
+      then from beginning to END of deque */
       else{
         for (int i = start; i < data.length; i++){
           dataStr += data[i] + ", ";
@@ -62,67 +62,84 @@ public class MyDeque<E>{
 
   // add element to beginning of deque
   public void addFirst(E element){
-    if (start == 0 && size > 0 && size < data.length){
-      start = data.length - 1;
-    }
-    else if (start > 0 && size < data.length){
-      start--;
-    }
-    else if (size >= data.length){
-  //    System.out.println("resize to add "+element);
+    // if array is full, resize array and set START as the last index of the array
+    if (size >= data.length){
       resize(data);
       start = data.length - 1;
     }
+    else{
+      // if there's one element in the array at index 0, move START to the last index of the array
+      if (start == 0 && size > 0 ){
+        start = data.length - 1;
+      }
+      // otherwise, move START one index left
+      else if (start > 0){
+        start--;
+      }
+    }
+    // set value of array at START as the given element and update size
     data[start] = element;
     size++;
-  //  System.out.println("start: "+start);
-  //  System.out.println("end: "+end);
+
   }
 
+  // add element to the end of the deque
   public void addLast(E element){
+    // if array is full, resize array and update END
     if (size >= data.length){
-    //  System.out.println("resize to add "+element);
       resize(data);
-
       end++;
     }
     else{
+      // if there's at least one element in the deque, update end
       if (size > 0){
         end++;
       }
+      // if end of array is reached, "loop" END around to the beginning of the array
       if (end >= data.length){
         end = 0;
       }
     }
+    // set value of array at END as the given element and update size
     data[end] = element;
     size++;
-  //  System.out.println("element: "+element+" start: "+start);
-  //  System.out.println("end: "+end);
   }
- public E removeFirst(){
-   if (size == 0){
-     throw new NoSuchElementException();
-   }
-   E old = data[start];
 
-     data[start] = null;
+  // remove element from the beginning of the deque
+  public E removeFirst(){
+    // if deque is empty, throw exception
+    if (size == 0){
+      throw new NoSuchElementException();
+    }
+
+    // store old value at the START and reset value of array at START
+    E old = data[start];
+    data[start] = null;
+
+    // update START by moving it by one to the right, or "looping" around to the beginning of the array
     if (start <= end || start < data.length - 1){
       start++;
     }
     else{
       start = 0;
     }
+    // update size and return the old element
     size--;
+    return old;
+  }
 
-   System.out.println("start now: "+start);
-   return old;
- }
+  // remove element from end of the deque
   public E removeLast(){
+      // if deque is empty, throw exception
     if (size == 0){
       throw new NoSuchElementException();
     }
 
-      E old = data[end];
+    // store old value at the END and reset value of array at END
+    E old = data[end];
+    data[end] = null;
+
+    // update END by moving it one to the left or "looping" around the array
     if (end >= data.length - 1){
       end = 0;
     }
@@ -132,22 +149,29 @@ public class MyDeque<E>{
     else{
       end--;
     }
+    // update size and return old element
     size--;
-    
-
     return old;
   }
   //public E getFirst(){ return element;}
   //public E getLast(){return element; }
+
+  // resize array by copying over values to a new array with double capacity
   private void resize(E[] array){
+    // make new array with double capacity
     @SuppressWarnings("unchecked")
     E[] newAry = (E[])new Object[array.length * 2];
+
+    // copy over all the values of the array if start is less than end
     if (start <= end){
       for (int i = start; i <= end; i++){
         newAry[i] = array[i];
       }
     }
     else{
+      /* If the values of the array loop around,
+      copy from START to end of the array, then from index 0 to END of the array
+      */
       int index = 0;
       for (int i = start; i < data.length; i++){
         newAry[index] = array[i];
@@ -160,10 +184,11 @@ public class MyDeque<E>{
         index++;
       }
     }
+
+    // set new array as data, reset start and end
     data = newAry;
     start = 0;
     end = size() - 1;
-  //  System.out.println("end after resize: "+end);
-  //  System.out.println(toString());
   }
+  
 }
